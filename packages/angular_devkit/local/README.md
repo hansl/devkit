31a29a32faf0572502a40ad4d6d8cff2cd22999e
Hans <hans@hansl.ca>
2018-03-27 13:28:39 -0700

    feat(@angular-devkit/local): add local package

---
+++ packages/angular_devkit/local/README.md
@@ -0,0 +1,3 @@
+# Local CLI Package
+
+Work in progress
---
+++ packages/angular_devkit/local/src/index.ts
@@ -0,0 +1,20 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import * as architect from '@angular-devkit/architect';
+import * as core from '@angular-devkit/core';
+import * as coreNode from '@angular-devkit/core/node';
+import * as schematics from '@angular-devkit/schematics';
+import * as schematicsTesting from '@angular-devkit/schematics/testing';
+
+export {
+  architect,
+  schematics,
+  core,
+  coreNode,
+  schematicsTesting,
+};
